1ee5a918556e74691e405ed7028716bdcba3981b
tianyongpeng <845632422@qq.com>
2018-01-20 20:02:18 +0800

    add Observer Pattern

---
+++ src/main/com.cn.patterns/observerpattern/Subject.java
@@ -0,0 +1,12 @@
+package observerpattern;
+
+public interface Subject {
+    //添加观察者
+    void addObserver(Observer observer);
+
+    //移除观察者
+    void deleteObserver(Observer observer);
+
+    //当主题方法改变时,这个方法被调用,通知所有的观察者
+    void notifyObserver();
+}
